9b52f3fe2839f74ef94af010baf488c14095b5f3
Voichita Maria <sabo_voichita@yahoo.com>
2026-01-28 14:52:57 +0100

    the logic of the game

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ optionImages.forEach((image, index) => {
 
     //loop through each option element again
     optionImages.forEach((image2, index2) => {
-      console.log((index, index2));
+      // console.log((index, index2));
       //if the current index doesn't match the clicked index
       //Remove the "active" class from the other option images
       index != index2 && image2.classList.remove("active");
@@ -26,6 +26,42 @@ optionImages.forEach((image, index) => {
     let imageSrc = e.target.querySelector("img").src;
     //Set the user image to the clicked option image
     userResult.src = imageSrc;
-    console.log(imageSrc);
+    // console.log(imageSrc);
+
+    //Generate a random number between 0 and 2
+    let randomNumber = Math.floor(Math.random() * 3);
+    // console.log(randomNumber);
+    //Create an array of CPU image options
+    let cpuImages = [
+      "images/rock.png",
+      "images/paper.png",
+      "images/scissors.png",
+    ];
+    //Set the CPU image to a random option from the array
+    cpuResult.src = cpuImages[randomNumber];
+
+    //Assign a letter to the CPU option("R" for rock, "P" for paper, "S" for scissors)
+    let cpuValue = ["R", "P", "S"][randomNumber];
+
+    //Assign a letter value to the clicked option (based on index)
+    let userValue = ["R", "P", "S"][index];
+
+    //Create an object with all the possible outcomes
+    let outcomes = {
+      RR: "Draw",
+      RP: "CPU",
+      RS: "YOU",
+      PP: "Draw",
+      PR: "YOU",
+      PS: "CPU",
+      SS: "Draw",
+      SR: "CPU",
+      SP: "YOU",
+    };
+
+    //Lookup the outcome value based on user and CPU options
+    let outComeValue = outcomes[userValue + cpuValue];
+    // console.log(cpuValue, userValue);
+    console.log(outComeValue);
   });
 });
