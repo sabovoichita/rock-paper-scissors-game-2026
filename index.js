d29ac2ccaed25fc9c154f0604deac3f0d18d0a4e
Voichita Maria <sabo_voichita@yahoo.com>
2026-01-28 14:36:34 +0100

    loop through to check if the image clicked index matches the option image

--- index.js
+++ index.js
@@ -5,3 +5,27 @@ const cpuResult = document.querySelector(".cpuResult img");
 const result = document.querySelector(".result");
 const optionImages = document.querySelectorAll(".optionImage");
 // console.log(gameContainer, userResult, cpuResult, result, optionImages);
+
+//loop through each option image element
+optionImages.forEach((image, index) => {
+  image.addEventListener("click", (e) => {
+    // console.log("image clicked", image, index);
+    image.classList.add("active");
+
+    //loop through each option element again
+    optionImages.forEach((image2, index2) => {
+      console.log((index, index2));
+      //if the current index doesn't match the clicked index
+      //Remove the "active" class from the other option images
+      index != index2 && image2.classList.remove("active");
+
+      // console.log(e.target);
+    });
+
+    //Get the source of the clicked option image
+    let imageSrc = e.target.querySelector("img").src;
+    //Set the user image to the clicked option image
+    userResult.src = imageSrc;
+    console.log(imageSrc);
+  });
+});
